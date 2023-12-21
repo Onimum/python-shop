6865350bef857ff470ac4b5158d1e5757ec47f4b
Onimum <tilekkushtar1@gmail.com>
2023-12-21 15:39:43 +0600

    upload image

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 
@@ -8,3 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("myapp/", include("myapp.urls", namespace="myapp"))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
